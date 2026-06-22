93376df8115c54299c630eae39f456d652c149fe
arek <arek.soulahian@gmail.com>
2026-06-21 17:24:38 -0700

    rm unneccessary dependencies

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
-const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 
@@ -11,7 +9,6 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,7 +19,9 @@ app.use('/favicon.ico', express.static('public/favicon.ico'));
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
